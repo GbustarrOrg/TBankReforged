069ad2e36fe5050f2a9f4a3828502bc71d6da249
ArturoAlvarezz <arturoalvareezz@gmail.com>
2023-12-19 21:31:23 -0300

    Implementando Jest

--- src/frontend/vitest.config.js
+++ src/frontend/vitest.config.js
@@ -6,7 +6,7 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
-      environment: 'jsdom',
+      environment: 'jsdom', 
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
